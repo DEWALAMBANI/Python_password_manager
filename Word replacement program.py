3eb0e065900fa8461f98392bd3806b99a92c9a20
DEWALAMBANI <91776960+DEWALAMBANI@users.noreply.github.com>
2023-07-31 12:32:42 +0530

    Add files via upload

---
+++ Word replacement program.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "hello everyone"
+    word_to_replace = input("enter the word to replace: ")
+    word_replacement = input("enter the word replacement: ")
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
